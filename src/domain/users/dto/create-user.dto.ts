87d3bee40c18f5e79de3a42ec08a8bcbd0525672
Alsainey Barry <alsainey7@gmail.com>
2025-07-29 01:09:42 +0000

    Validation set up

--- src/domain/users/dto/create-user.dto.ts
+++ src/domain/users/dto/create-user.dto.ts
@@ -1,6 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly email: string;
+  @IsString()
   readonly phone: string;
+  @IsString()
   readonly password: string;
 }
