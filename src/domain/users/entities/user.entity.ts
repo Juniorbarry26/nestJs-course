3bf48ce1376880d76bc6077733d0b2fa20d1003c
Alsainey Barry <alsainey7@gmail.com>
2025-08-02 14:31:51 +0000

    minor changes

--- src/domain/users/entities/user.entity.ts
+++ src/domain/users/entities/user.entity.ts
@@ -1,7 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { RegistryDate } from '../../../common/embedded/registry-date.embedded';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
   name: string;
+  @Column({ unique: true })
   email: string;
+  @Column({ unique: true })
   phone: string;
+  @Column()
   password: string;
+
+  @Column(() => RegistryDate, { prefix: false })
+  registryDate: RegistryDate;
 }
