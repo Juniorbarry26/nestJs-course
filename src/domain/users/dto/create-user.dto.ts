f7b201d8319a9422070404e76b22d8b35a8e17f1
Alsainey Barry <alsainey7@gmail.com>
2025-08-18 12:38:22 +0000

    validating password with regex

--- src/domain/users/dto/create-user.dto.ts
+++ src/domain/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsString } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -8,6 +8,12 @@ export class CreateUserDto {
   readonly email: string;
   @IsString()
   readonly phone: string;
-  @IsString()
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password too weak. Must contain uppercase, lowercase, number, special char, and be at least 8 characters.',
+    },
+  )
   readonly password: string;
 }
