8cd246792730636541a0d350578c4be61bcef700
Alsainey Barry <alsainey7@gmail.com>
2025-08-19 06:55:23 +0000

    using event listeners to hash password

---
+++ src/domain/users/subscribers/user.subscriber.ts
@@ -0,0 +1,35 @@
+import {
+  DataSource,
+  EntitySubscriberInterface,
+  InsertEvent,
+  UpdateEvent,
+} from 'typeorm';
+import { HashingService } from '../../../auth/hashing/hashing.service';
+import { User } from '../entities/user.entity';
+
+export class UserSubscriber implements EntitySubscriberInterface<User> {
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly hashingService: HashingService,
+  ) {
+    dataSource.subscribers.push(this);
+  }
+  listenTo() {
+    return User;
+  }
+
+  async beforeInsert(event: InsertEvent<User>) {
+    const { entity: user } = event;
+
+    user.password = await this.hashingService.hash(user.password);
+  }
+
+  async beforeUpdate(event: UpdateEvent<User>) {
+    const { entity, databaseEntity: databaseUser } = event;
+    const user = entity as User;
+
+    if (user.password !== databaseUser.password) {
+      user.password = await this.hashingService.hash(user.password);
+    }
+  }
+}
